f9d339937ec218fa9dbc128bc475a818ba3cc2be
SayuriRK <renatasayuri22@gmail.com>
2021-09-17 18:47:21 -0300

    Testing the top words code

--- word_count.py
+++ word_count.py
@@ -3,5 +3,6 @@ from pyspark import SparkContext, SparkConf
 if __name__ == "__main__":
     sc = SparkContext("local","PySpark Exemplo - Desafio Dataproc")
     words = sc.textFile("gs://dataproc-word-count/livro.txt").flatMap(lambda line: line.split(" "))
-    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False).take(10)
-    wordCounts.saveAsTextFile("gs://dataproc-word-count/results")
+    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False)
+    topWords= sc.parallelize(wordCounts.take(10))
+    topWords.saveAsTextFile("gs://dataproc-word-count/results")
