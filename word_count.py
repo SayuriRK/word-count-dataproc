ce8856d7bbc3f69324d8f487ae5f5a35b90a231c
SayuriRK <renatasayuri22@gmail.com>
2021-09-17 18:27:41 -0300

    Changed bucket's name

--- word_count.py
+++ word_count.py
@@ -2,6 +2,6 @@ import sys
 from pyspark import SparkContext, SparkConf
 if __name__ == "__main__":
     sc = SparkContext("local","PySpark Exemplo - Desafio Dataproc")
-    words = sc.textFile("gs://dataproc/livro.txt").flatMap(lambda line: line.split(" "))
+    words = sc.textFile("gs://dataproc-word-count/livro.txt").flatMap(lambda line: line.split(" "))
     wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False).take(10)
-    wordCounts.saveAsTextFile("gs://dataproc/results")
+    wordCounts.saveAsTextFile("gs://dataproc-word-count/results")
