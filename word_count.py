9706d8d21d85842115fd717cd06d4f5e8539c5b4
SayuriRK <renatasayuri22@gmail.com>
2021-09-17 17:20:26 -0300

    Testing

--- word_count.py
+++ word_count.py
@@ -3,5 +3,5 @@ from pyspark import SparkContext, SparkConf
 if __name__ == "__main__":
     sc = SparkContext("local","PySpark Exemplo - Desafio Dataproc")
     words = sc.textFile("gs://dataproc-project-word-count/livro.txt").flatMap(lambda line: line.split(" "))
-    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False).take(20)
+    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False)
     wordCounts.saveAsTextFile("gs://dataproc-project-word-count/results")
