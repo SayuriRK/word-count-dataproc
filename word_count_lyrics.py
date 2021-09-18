3de7393cfb83e2bb6f3b5ba926eb76cc9e68949c
Renata Sayuri <86317760+SayuriRK@users.noreply.github.com>
2021-09-18 17:38:48 -0300

    Delete word_count_lyrics.py

--- word_count_lyrics.py
+++
@@ -1,8 +0,0 @@
-import sys
-from pyspark import SparkContext, SparkConf
-if __name__ == "__main__":
-    sc = SparkContext("local","PySpark Exemplo - Desafio Dataproc")
-    words = sc.read.json("gs://dataproc-word-count/song_lyrics_genius.jl").flatMap(lambda line: line.split(" "))
-    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False)
-    topWords= sc.parallelize(wordCounts.take(15))
-    topWords.saveAsTextFile("gs://dataproc-word-count/results_lyrics")
