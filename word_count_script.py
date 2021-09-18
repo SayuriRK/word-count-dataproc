5ee054736376fd09c7867756d74abe539053ba61
Renata Sayuri <86317760+SayuriRK@users.noreply.github.com>
2021-09-18 18:01:58 -0300

    Delete word_count_script.py

--- word_count_script.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from pyspark import SparkContext, SparkConf
-if __name__ == "__main__":
-    sc = SparkContext("local","PySpark Exemplo - Desafio Dataproc")
-    words = sc.textFile("gs://dataproc-word-count/thor_ragnarok_script.txt").flatMap(lambda line: line.split(" "))
-    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False)
-    topWords= sc.parallelize(wordCounts.take(20))
-    topWords.saveAsTextFile("gs://dataproc-word-count/results_script")
